834dcaf0b68d3a37de7e0a9feaeb499a1eef94fa
Arthur - BeeDo <arthur@beedotech.com.br>
2025-08-21 18:36:20 -0300

    feat: Completa a remoção de Clerk/Prisma/Dashboard e corrige o deploy

--- src/components/global/providers.tsx
+++ src/components/global/providers.tsx
@@ -1,23 +1,21 @@
 "use client";
 
 import React from "react"
-import { ClerkProvider } from "@clerk/nextjs"
-// import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "sonner";
+import { ThemeProvider } from "next-themes";
 
-interface Props {
-    children: React.ReactNode;
-}
-
-// const client = new QueryClient();
-
-const Providers = ({ children }: Props) => {
+const Providers = ({ children }: { children: React.ReactNode }) => {
     return (
-        // <QueryClientProvider client={client}>
-        <ClerkProvider>
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+        >
             {children}
-        </ClerkProvider>
-        // </QueryClientProvider> 
-    );
+            <Toaster position="top-center" richColors />
+        </ThemeProvider>
+    )
 };
 
 export default Providers
